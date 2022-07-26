ce357b644d81845ba80f893bffc749236833b87b
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-26 16:51:27 -0400

    Clase 2: React.Component vs. useState

--- src/UseState.js
+++ src/UseState.js
@@ -1,10 +1,14 @@
-export function UseState() {
+import { useState } from "react";
+
+export function UseState(props) {
+  const [error, setError] = useState(false);
   return (
     <div>
-      <h2>Eliminar UseState</h2>
+      <h2>Eliminar {props.name}</h2>
       <p>Por favor, escribe el código de seguridad.</p>
+      {error && <p>Error: El código es incorrecto</p>}
       <input placeholder="Código de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
