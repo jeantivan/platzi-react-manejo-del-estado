d95f36412240d7aee64ceda3efadb7f60e6c1a9b
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-26 17:01:28 -0400

    Clase 3: Efectos con useEffect

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function UseState(props) {
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
+  }, [loading]);
+
   return (
     <div>
       <h2>Eliminar {props.name}</h2>
+
       <p>Por favor, escribe el código de seguridad.</p>
+
       {error && <p>Error: El código es incorrecto</p>}
+
+      {loading && <p>Cargando...</p>}
+
       <input placeholder="Código de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+
+      <button disabled={loading} onClick={() => setLoading(true)}>
+        Comprobar
+      </button>
     </div>
   );
 }
