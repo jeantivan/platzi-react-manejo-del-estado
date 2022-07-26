bbabcc7de2c722248434d879a05d086f0a00e145
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-26 16:38:44 -0400

    Clase 1: Iniciando el proyecto

---
+++ src/ClassState.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class ClassState extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Eliminar ClassState</h2>
+        <p>Por favor, escribe el código de seguridad.</p>
+        <input placeholder="Código de seguridad" />
+        <button>Comprobar</button>
+      </div>
+    );
+  }
+}
---
+++ src/UseState.js
@@ -0,0 +1,10 @@
+export function UseState() {
+  return (
+    <div>
+      <h2>Eliminar UseState</h2>
+      <p>Por favor, escribe el código de seguridad.</p>
+      <input placeholder="Código de seguridad" />
+      <button>Comprobar</button>
+    </div>
+  );
+}
