2953197f146c1fab21395134131777ec47faf541
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-27 14:20:56 -0400

    Clase 6: Estados compuestos con React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Loading } from "./Loading";
+
+const SECURITY_CODE = "paradigma";
+
 export class ClassState extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      value: "",
       error: false,
       loading: false,
     };
@@ -22,19 +26,31 @@ export class ClassState extends React.Component {
   componentDidUpdate() {
     if (this.state.loading) {
       setTimeout(() => {
-        this.setState({ loading: false });
+        if (SECURITY_CODE === this.state.value) {
+          this.setState({ loading: false });
+        } else {
+          this.setState({ error: true, loading: false });
+        }
       }, 3000);
     }
   }
   render() {
+    const { error, loading, value } = this.state;
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escribe el código de seguridad.</p>
-        {this.state.error && <p>Error: El código es incorrecto</p>}
-        {this.state.loading && <Loading />}
-        <input placeholder="Código de seguridad" />
+        {error && !loading && <p>Error: El código es incorrecto</p>}
+        {loading && <Loading />}
+        <input
+          value={value}
+          onChange={(e) => {
+            this.setState({ value: e.target.value });
+          }}
+          placeholder="Código de seguridad"
+        />
         <button
+          disabled={loading}
           onClick={() => {
             this.setState({ loading: true });
           }}
