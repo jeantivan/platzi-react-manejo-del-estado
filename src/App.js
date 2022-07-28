0101173eb057530c2ab1118769164146ec35f244
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-28 17:42:18 -0400

    Clase 10: Estados declarativos con useReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { UseState } from "./UseState";
 import { ClassState } from "./ClassState";
+import { UseReducer } from "./UseReducer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <UseState name="UseState" />
+      <UseReducer name="UseReducer" />
       <ClassState name="ClassState" />
     </div>
   );
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,3 +1,5 @@
+import { useReducer, useEffect } from "react";
+
 const INITIAL_STATE = {
   value: "",
   error: false,
@@ -6,64 +8,123 @@ const INITIAL_STATE = {
   confirmed: false,
 };
 
-// const reducer = (state, action) => {
+const SECURITY_CODE = "paradigma";
 
-// }
-const reducerIf = (state, action) => {
-  if (action.type === "ERROR") {
-    return {
-      ...state,
-      error: true,
-      loading: false,
-    };
-  } else if (action.type === "CHECK") {
-    return {
-      ...state,
-      loading: true,
-    };
-  } else {
-    return {
-      ...INITIAL_STATE,
-    };
-  }
-};
-
-const reducerSwitch = (state, action) => {
-  switch (action.type) {
-    case "ERROR":
-      return {
-        ...state,
-        error: true,
-        loading: false,
-      };
-    case "CHECK":
-      return {
-        ...state,
-        loading: true,
-      };
-    default:
-      return {
-        ...state,
-      };
-  }
-};
-
-const reducerObject = (state) => ({
+const reducerObject = (state, payload) => ({
   ERROR: {
     ...state,
     error: true,
     loading: false,
   },
-  CHECK: {
+  CHECK: { ...state, loading: true },
+  CONFIRM: {
     ...state,
-    loading: true,
+    error: false,
+    loading: false,
+    confirmed: true,
   },
+  DELETE: { ...state, deleted: true },
+  RESET: { ...INITIAL_STATE },
+  WRITE: { ...state, value: payload },
 });
 
 const reducer = (state, action) => {
   if (reducerObject(state)[action.type]) {
-    return reducerObject(state)[action.type];
+    return reducerObject(state, action.payload)[action.type];
   }
 
   return state;
 };
+
+export function UseReducer(props) {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  const onWrite = (e) => {
+    //setState({ ...state, value: e.target.value });
+  };
+
+  useEffect(() => {
+    if (state.loading) {
+      setTimeout(() => {
+        if (state.value === SECURITY_CODE) {
+          dispatch({
+            type: "CONFIRM",
+          });
+        } else {
+          dispatch({
+            type: "ERROR",
+          });
+        }
+      }, 3000);
+    }
+  }, [state.loading]);
+
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {props.name}</h2>
+
+        <p>Por favor, escribe el código de seguridad.</p>
+
+        {state.error && !state.loading && <p>Error: El código es incorrecto</p>}
+
+        {state.loading && <p>Cargando...</p>}
+
+        <input
+          value={state.value}
+          onChange={(event) => {
+            dispatch({
+              type: "WRITE",
+              payload: event.target.value,
+            });
+          }}
+          placeholder="Código de seguridad"
+        />
+
+        <button
+          disabled={state.loading}
+          onClick={() => {
+            dispatch({ type: "CHECK" });
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <div>
+        <h2>Eliminar {props.name}</h2>
+        <p>Pedimos confirmación. ¿Tas seguro?</p>
+        <button
+          onClick={() => {
+            dispatch({ type: "DELETE" });
+          }}
+        >
+          Confirmar
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "RESET" });
+          }}
+        >
+          Cancelar
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>Eliminado con éxito</p>
+        <button
+          onClick={() => {
+            dispatch({ type: "RESET" });
+          }}
+        >
+          Volver al inicio
+        </button>
+      </div>
+    );
+  }
+}
